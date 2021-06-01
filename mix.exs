6d2bd961c45bb58a8b3fd66d5db99bb400ae4951
Philipp <git@pentacent.com>
2021-06-01 23:56:47 +0200

    Bump version number to v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixLiveSession.MixProject do
   def project do
     [
       app: :phoenix_live_session,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
